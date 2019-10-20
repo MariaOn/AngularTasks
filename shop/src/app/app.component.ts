88815f02c6d08468c5ce8c7f73a77e66415b728a
MariaOn <onufrievich-maria@ukr.net>
2019-10-21 01:49:00 +0300

    test

--- shop/src/app/app.component.ts
+++ shop/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FirstComponentComponent } from './first-component/first-component.component';
+
 
 @Component({
   selector: 'app-root',
--- shop/src/app/app.module.ts
+++ shop/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { ProductComponentComponent } from './product-component/product-component.component';
+import { CardComponent } from './card/card.component';
+import { ProductListComponentComponent } from './product-list-component/product-list-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    ProductComponentComponent,
+    CardComponent,
+    ProductListComponentComponent
   ],
   imports: [
     BrowserModule,

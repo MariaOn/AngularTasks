82a97dedd7711bdd24902981c8766470c5477852
Mariia Onufriievych <Mariia_Onufriievych@epam.com>
2019-10-30 21:06:33 +0200

    task 1

--- shop/src/app/app.module.ts
+++ shop/src/app/app.module.ts
@@ -3,18 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstComponentComponent } from './first-component/first-component.component';
-import { ProductComponentComponent } from './product-component/product-component.component';
-import { CardComponent } from './card/card.component';
-import { ProductListComponentComponent } from './product-list-component/product-list-component.component';
+import { FirstComponentComponent } from './products/components/first-component/first-component.component';
+import { ProductComponentComponent } from './products/components/product-component/product-component.component';
+import { ProductListComponentComponent } from './products/components/product-list-component/product-list-component.component';
+import { CartComponent } from './carts/components/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
     ProductComponentComponent,
-    CardComponent,
-    ProductListComponentComponent
+    ProductListComponentComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
